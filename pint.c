9ed019dac1cd09d0cf97cad17f83fad3c168661a
yoyogold-a11 <tmrfack@gmail.com>
2021-05-12 21:00:57 +0000

    fixing code

--- pint.c
+++ pint.c
@@ -13,7 +13,7 @@ void pint(stack_t **stack, unsigned int line_number)
 
 	if (var.stack_len == 0)
 	{
-		dprintf(STDERR_FILENO, "L%u: can't pint, stack empty\n", line_number);
+		dprintf(STDOUT_FILENO, "L%u: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	printf("%d\n", tmp->n);
