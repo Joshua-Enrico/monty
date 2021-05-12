78276dee8024a2385d73c357fed4e9440891290b
yoyogold-a11 <tmrfack@gmail.com>
2021-05-12 23:32:24 +0000

    fixing code

--- div.c
+++ div.c
@@ -13,7 +13,7 @@ void div_m(stack_t **stack, unsigned int line_number)
 
 	if (var.stack_len < 2)
 	{
-		dprintf(STDOUT_FILENO,
+		dprintf(STDERR_FILENO,
 			"L%u: can't div, stack too short\n",
 			line_number);
 		exit(EXIT_FAILURE);
@@ -22,7 +22,7 @@ void div_m(stack_t **stack, unsigned int line_number)
 	pop(stack, line_number);
 	if (n == 0)
 	{
-		dprintf(STDOUT_FILENO,
+		dprintf(STDERR_FILENO,
 			"L%u: division by zero\n",
 			line_number);
 		exit(EXIT_FAILURE);
