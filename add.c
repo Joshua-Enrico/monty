8cfb8965b1875f1774317c1a166b087fa74b4822
yoyogold-a11 <tmrfack@gmail.com>
2021-05-12 23:06:57 +0000

    fixing code

--- add.c
+++ add.c
@@ -9,18 +9,14 @@
  */
 void add(stack_t **stack, unsigned int line_number)
 {
-	stack_t *tmp = *stack;
-	int sum = 0;
+	int n = 0;
 
 	if (var.stack_len < 2)
 	{
 		dprintf(STDERR_FILENO, "L%u: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	tmp = *stack;
-	sum = tmp->n + tmp->next->n;
-	tmp->next->n = sum;
+	n += (*stack)->n;
 	pop(stack, line_number);
-
-	var.stack_len--;
+	(*stack)->n += n;
 }
