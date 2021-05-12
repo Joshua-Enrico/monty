2228a0876a58df4f7bc731a1c0bbb9319ab9c7d0
yoyogold-a11 <tmrfack@gmail.com>
2021-05-12 20:54:53 +0000

    fixing code

--- main.c
+++ main.c
@@ -10,36 +10,29 @@
 
 int main(int argc, char *argv[])
 {
-	/* declaring needed variables*/
-	char *lineptr = NULL, *op = NULL;
-	FILE *file = NULL;
-	size_t n = 0;
-	/* var to get lines numbers */
-	unsigned int line_number = 0;
-	/* declarin stack that  will be used*/
 	stack_t *stack = NULL;
+	unsigned int line_number = 0;
+	FILE *fs = NULL;
+	char *lineptr = NULL, *op = NULL;
+	size_t n = 0;
 
 	var.queue = 0;
 	var.stack_len = 0;
-
 	if (argc != 2)
 	{
 		dprintf(STDERR_FILENO, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-	/* Here we read the given file*/
-	file = fopen(argv[1], "r");
-	if (file == NULL)
+	fs = fopen(argv[1], "r");
+	if (fs == NULL)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-	/*here we need to end and close some processes on exit */
 	on_exit(free_lineptr, &lineptr);
 	on_exit(free_stack, &stack);
-	on_exit(close_File, &lineptr);
-	/* here we fork and passes arguments to match a op*/
-	while (getline(&lineptr, &n, file) != -1)
+	on_exit(close_File, fs);
+	while (getline(&lineptr, &n, fs) != -1)
 	{
 		line_number++;
 		op = strtok(lineptr, "\n\t\r ");
@@ -47,7 +40,6 @@ int main(int argc, char *argv[])
 		{
 			check_if_op_match(op, &stack, line_number);
 		}
-
 	}
-	   exit(EXIT_SUCCESS);
+	exit(EXIT_SUCCESS);
 }
