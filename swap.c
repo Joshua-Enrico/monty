046f10ac80fcf1465b2bfe5e7b38ce3aa919e4cc
yoyogold-a11 <tmrfack@gmail.com>
2021-05-12 23:03:52 +0000

    fixing code

--- swap.c
+++ swap.c
@@ -9,28 +9,26 @@
  */
 void swap(stack_t **stack, unsigned int line_number)
 {
-	stack_t *tmp = *stack;
+	stack_t *next;
 
 	if (var.stack_len < 2)
 	{
-		dprintf(STDERR_FILENO, "L%u: can't swap, stack too short\n", line_number);
+		dprintf(STDERR_FILENO,
+			"L%u: can't swap, stack too short\n",
+			line_number);
 		exit(EXIT_FAILURE);
 	}
-		if (var.stack_len == 2)
+	if (var.stack_len == 2)
 	{
 		*stack = (*stack)->next;
 		return;
 	}
-	else
-	{
-		tmp->prev->next = tmp->next;
-		tmp->next->prev = tmp->prev;
-		tmp->next->next->prev = tmp;
-		tmp->prev = tmp->next;
-		tmp->next = tmp->next->next;
-		tmp->prev->next = tmp;
-
-		*stack = tmp->prev;
-	}
-
+	next = (*stack)->next;
+	next->prev = (*stack)->prev;
+	(*stack)->prev->next = next;
+	(*stack)->prev = next;
+	(*stack)->next = next->next;
+	next->next->prev = *stack;
+	next->next = *stack;
+	*stack = next;
 }
