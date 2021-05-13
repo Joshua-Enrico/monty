b9fc05eeeadc0b58153aaa5099fdc023e94d09fe
yoyogold-a11 <tmrfack@gmail.com>
2021-05-13 02:07:50 +0000

    testing

--- pall.c
+++ pall.c
@@ -15,7 +15,7 @@ void pall(stack_t **stack, unsigned int line_number)
 	head = *stack;
 	while (head != NULL)
 	{
-		printf("%d\n", head->n);
+		dprintf(STDOUT_FILENO, "%d\n", head->n);
 		head = head->next;
 		if (head == *stack)
 		{
