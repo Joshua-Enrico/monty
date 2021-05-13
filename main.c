bd788fca33fc775029f895e835967a0dd8e415d2
yoyogold-a11 <tmrfack@gmail.com>
2021-05-13 04:34:42 +0000

    testing

--- main.c
+++ main.c
@@ -30,6 +30,7 @@ int main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 	on_exit(free_lineptr, &lineptr);
+	on_exit(free_stack, &stack);
 	on_exit(close_File, fs);
 	while (getline(&lineptr, &n, fs) != -1)
 	{
