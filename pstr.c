86f50b4f5b508672a655a96e20b99b63b32bb104
yoyogold-a11 <tmrfack@gmail.com>
2021-05-13 06:31:34 +0000

    fixing code

--- pstr.c
+++ pstr.c
@@ -1,15 +1,13 @@
 #include "monty.h"
 #include <ctype.h>
-
 /**
- * m_pstr - print string from stack of ints up to null byte,
- * first non-ascii character, or end of stack
+ * pstr - prints the string starting at the top of the stack.
  * @stack: double pointer to head of stack
  * @line_number: line number of current operation
  *
  * Return: void
  */
-void m_pstr(stack_t **stack, unsigned int line_number)
+void pstr(stack_t **stack, unsigned int line_number)
 {
 	stack_t *temp;
 	int ch;
