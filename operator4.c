d5697b8bb7901669168a99d9cb1507e79795a252
yoyogold-a11 <tmrfack@gmail.com>
2021-05-14 07:07:39 +0000

    fixing

--- operator4.c
+++ operator4.c
@@ -3,22 +3,22 @@
 /**
  * mul - multiplies the second top element of the stack with the top element.
  * @stack: double pointer to head of stack
- * @line_number: line number of current operation
+ * @Linenumber: line number of current operation
  *
  * Return: void
  */
-void mul(stack_t **stack, unsigned int line_number)
+void mul(stack_t **stack, unsigned int Linenumber)
 {
 	stack_t *tmp = *stack;
 	int sub = 0;
 
 	if (var.stack_len < 2)
 	{
-		dprintf(STDERR_FILENO, "L%u: can't mul, stack too short\n", line_number);
+		dprintf(STDERR_FILENO, "L%u: can't mul, stack too short\n", Linenumber);
 		exit(EXIT_FAILURE);
 	}
 	tmp = *stack;
 	sub = tmp->next->n * tmp->n;
 	tmp->next->n = sub;
-	pop(stack, line_number);
+	pop(stack, Linenumber);
 }
